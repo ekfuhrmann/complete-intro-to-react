b9715c93f0284ff4011925488a6cd5c5f8fe7c81
Eric Fuhrmann <ekfuhrmann@gmail.com>
2018-02-05 22:49:57 -0500

    Finished configuring jest

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -7,15 +7,15 @@ import Search from './Search';
 const FourOhFour = () => <h1>404</h1>;
 
 const App = () => (
-    <BrowserRouter>
-        <div className="app">
-            <Switch>
-                <Route exact path="/" component={Landing} />
-                <Route path="/search" component={Search} />
-                <Route component={FourOhFour} />
-            </Switch>
-        </div>
-    </BrowserRouter>
+  <BrowserRouter>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/search" component={Search} />
+        <Route component={FourOhFour} />
+      </Switch>
+    </div>
+  </BrowserRouter>
 );
 
 render(<App />, document.getElementById('app'));
--- js/Search.jsx
+++ js/Search.jsx
@@ -3,30 +3,37 @@ import ShowCard from './ShowCard';
 import preload from '../data.json';
 
 class Search extends Component {
-    state = {
-        searchTerm: ''
-    };
-    handelSearchTermChange = event => {
-        this.setState({ searchTerm: event.target.value });
-    };
-    render() {
-        return (
-            <div className="search">
-                <header>
-                    <h1>svideo</h1>
-                    <input
-                        onChange={this.handelSearchTermChange}
-                        value={this.state.searchTerm}
-                        type="text"
-                        placeholder="search"
-                    />
-                </header>
-                <div>
-                    {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
-                </div>
-            </div>
-        );
-    }
+  state = {
+    searchTerm: ''
+  };
+  handleSearchTermChange = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {preload.shows
+            .filter(
+              show =>
+                `${show.title} ${show.description}`
+                  .toUpperCase()
+                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Search;
